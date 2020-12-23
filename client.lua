cec6eaffc5dddaed8d28bf764b953e762f676da8
fribblet56 <fribblet56@gmail.com>
2020-12-23 22:33:41 +0100

    Init

---
+++ client.lua
@@ -0,0 +1,43 @@
+AddCommand("map", function(mapId)
+
+    mapId = tonumber(mapId)
+
+    local Map = Maps_List[mapId]
+
+    if GetWorld():GetMapName() ~= Map.pak.name then
+        
+        if not LoadPak(Map.pak.name,"/"..Map.pak.name.."/","../../../OnsetModding/Plugins/"..Map.pak.projetName.."/Content/") then
+            AddPlayerChat("[ERROR] Error then package of map "..Map.name.." loading, edit your config file !")
+            return
+        end
+
+        if not ConnectToServer(GetServerIP(),GetServerPort(), Server_Password, Map.pak.path) then
+            AddPlayerChat("[ERROR] Error then connect to "..Map.name.." edit your config file !")
+            return
+        end
+    else
+        AddPlayerChat("Your are already in "..Map.name.."!")
+    end
+end)
+
+
+AddEvent("OnPlayerSpawn", function(playerid)
+    for k,v in pairs(Maps_List) do
+        if GetWorld():GetMapName() == v.mapName then 
+            local plyactor = GetPlayerActor(GetPlayerId())
+            plyactor:SetActorLocation(FVector(v.spawn[1], v.spawn[2], v.spawn[3]))
+            return
+        end
+    end
+end)
+
+
+AddCommand("maplist", function()
+    AddPlayerChat("--------------------")
+
+    for k,v in pairs(Maps_List) do
+        AddPlayerChat("Name: "..v.name.." [Id:"..k.."]")
+    end
+
+    AddPlayerChat("--------------------")
+end)
---
+++ config.lua
@@ -0,0 +1,24 @@
+Server_Password = ""
+
+Maps_List = {
+    {
+        name = "Industrial",
+        mapName = "industrial_area",
+        spawn = {2400,-540,195},
+        pak = {
+            name = "Industrial_Area",
+            projetName = "Industrial_Area",
+            path = "/Industrial_Area/industrial_area/Maps/industrial_area"
+        }
+    },
+    {
+        name = "Factory",
+        mapName = "Main_Warehouse_02",
+        spawn = {67,236,201},
+        pak = {
+            name = "Factory",
+            projetName = "Factory",
+            path = "/Factory/AbandonedFactory/Maps/Warehouse_02_night/Main_Warehouse_02"
+        }
+    }
+}
